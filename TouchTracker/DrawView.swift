2e8d84f7a323d477d968f823f88ce320ccfe37c4
Savannah Stoughton <sstoughton@vision.local>
2019-04-10 13:18:33 -0400

    touch tracker app

---
+++ TouchTracker/DrawView.swift
@@ -0,0 +1,9 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Savannah Stoughton on 4/10/19.
+//  Copyright © 2019 Savannah Stoughton. All rights reserved.
+//
+
+import Foundation
---
+++ TouchTracker/Line.swift
@@ -0,0 +1,9 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Savannah Stoughton on 4/10/19.
+//  Copyright © 2019 Savannah Stoughton. All rights reserved.
+//
+
+import Foundation
